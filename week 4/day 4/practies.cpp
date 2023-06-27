8a9d75d62ef292eb9a9f7e2db1b8d4b2ed251acb
Soroar-Mia <miasoroar@gmail.com>
2023-06-27 16:26:48 +0600

    week-4 day-5

---
+++ week 4/day 4/practies.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int t, x, i, j, k, p, s, h, m;
+    cin >> t;
+    array<long long int, 2> n[100];
+
+    while(t--)
+    {
+        cin >> x >> h >> m;
+        for(i = 0; i < x; i++)
+            cin >> n[i][0] >> n[i][1];
+
+        sort(n, n + x);
+        s = 0;
+        for(i = 0; i < x; i++) {
+            k = n[i][0];
+            p = n[i][1];
+
+            if(k > h) {
+                if(m > p) {
+                    h++;
+                    p = p + (60 - m);
+                } else {
+                    p = p - m;
+                }
+
+                if(h >= 24)
+                    h = h - 24;
+
+                if(h > k) {
+                    k = k + (24 - h);
+                } else {
+                    k = k - h;
+                }
+
+                s = 1;
+                break;
+            }
+            else if(k == h && p >= m) {
+                k = 0;
+                p = p - m;
+                s = 1;
+                break;
+            }
+        }
+
+        if(s == 0) {
+            k = n[0][0];
+            p = n[0][1];
+
+            if(m > p) {
+                h++;
+                p = p + (60 - m);
+            } else {
+                p = p - m;
+            }
+
+            if(h >= 24)
+                h = h - 24;
+
+            if(h > k) {
+                k = k + (24 - h);
+            } else {
+                k = k - h;
+            }
+        }
+
+        cout << k << " " << p << endl;
+    }
+}
